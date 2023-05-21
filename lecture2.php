c3232f939d5a640462f811ce95dcdbc1866e35cb
Gvelesa07 <116925513+Gvelesa07@users.noreply.github.com>
2023-05-21 18:37:45 +0400

    Add files via upload

---
+++ lecture2.php
@@ -0,0 +1,25 @@
+<?php
+
+// Data types
+$y = ['Walter', 7, false ,['White',17.0]];
+var_dump($y [3] [1] );
+
+// Operators
+$x = 15;
+$y = 13;
+
+echo $x + $y;
+//echo $x - $y;
+//echo $x * $y;
+//echo $x / $y;
+//echo $x % $y;
+//echo $x ** $y;
+//echo $x = $y;
+//echo $x = $y;
+//echo $x += $y;
+//echo $x -= $y;
+//echo $x *= $y;
+//echo $x /= $y;
+//echo $x **= $y;
+//echo $x == $y; 
+//echo $x === $y; 
